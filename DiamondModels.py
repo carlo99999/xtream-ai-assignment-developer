a1111152806b3333f5ac073c885c96aa53d10cdb
carlopiccinin_2 <carlo.piccinin@gmail.com>
2024-06-21 17:14:28 +0200

    Added interface for pipeline

--- DiamondModels.py
+++ DiamondModels.py
@@ -4,7 +4,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
 import joblib
 import uuid
-import xgboost
+import xgboost as xgb
 from xgboost import XGBRegressor
 import os
 import matplotlib.pyplot as plt
@@ -12,6 +12,8 @@ import plotly.express as px
 import warnings
 import optuna
 from typing import Union
+import json
+import numpy as np
 
 file_readers = {
     '.csv': pd.read_csv,
@@ -32,6 +34,11 @@ file_readers = {
     '.gbq': pd.read_gbq
 }
 
+modelling_algorithms = {
+    "LinearRegression": LinearRegression,
+    "XGBRegressor": XGBRegressor
+}
+
 class DiamondModel:
     """
     Initialize the model either with data to train the model or an id to load an existing model.
@@ -43,40 +50,31 @@ class DiamondModel:
         - LinearRegression
         - XGBRegressor
     """
-    def __init__(self, datas: Union[pd.DataFrame, str]=None, id: str=None, model: str=None) -> None:
-        self.datas = None
-        self.model = None
-        self.modelling_algorithms = {
-            "LinearRegression": LinearRegression,
-            "XGBRegressor": XGBRegressor
-        }
-        self.id = uuid.uuid4().hex
-        self.model_name = model
-
+    def __init__(self, datas: Union[pd.DataFrame, str] = None, id: str = None, model: str = None) -> None:
         if datas is None and id is None:
             raise ValueError("You must provide data to train the model or an id to load the model")
         if id is None and model is None:
             raise ValueError("You must decide a model to train the data")
-        if model and model not in self.modelling_algorithms:
+        if model and model not in modelling_algorithms:
             raise ValueError("The model provided is not yet supported")
+        
+        self.id = id or uuid.uuid4().hex
+        self.model_name = model
+        self.model = modelling_algorithms.get(model)
         
         if isinstance(datas, str):
             self.datas = self._load_data_from_file(datas)
         elif isinstance(datas, pd.DataFrame):
             self.datas = datas
-
+        
         if id:
             self._load(id)
         
-        if self.datas is None:
+        if self.datas is None and datas is not None:
             raise ValueError("The data could not be loaded")
 
-        if model:
-            self.model = self.modelling_algorithms[model]()
-
     @staticmethod
-    def objective(x_train_xbg, y_train_xbg, trial: optuna.trial.Trial) -> float:
-        # Define hyperparameters to tune
+    def objective(x_train_xgb, y_train_xgb, trial: optuna.trial.Trial) -> float:
         param = {
             'lambda': trial.suggest_float('lambda', 1e-8, 1.0, log=True),
             'alpha': trial.suggest_float('alpha', 1e-8, 1.0, log=True),
@@ -90,25 +88,17 @@ class DiamondModel:
             'enable_categorical': True
         }
 
-        # Split the training data into training and validation sets
-        x_train, x_val, y_train, y_val = train_test_split(x_train_xbg, y_train_xbg, test_size=0.2, random_state=42)
-
-        # Train the model
-        model = xgboost.XGBRegressor(**param)
+        x_train, x_val, y_train, y_val = train_test_split(x_train_xgb, y_train_xgb, test_size=0.2, random_state=42)
+        model = xgb.XGBRegressor(**param)
         model.fit(x_train, y_train)
-
-        # Make predictions
         preds = model.predict(x_val)
-
-        # Calculate MAE
         mae = mean_absolute_error(y_val, preds)
-
         return mae
 
     @staticmethod
-    def find_best_hyperparameters(x_train_xbg, y_train_xbg, n_trials: int=100) -> dict:
+    def find_best_hyperparameters(x_train_xgb, y_train_xgb, n_trials: int = 100) -> dict:
         study = optuna.create_study(direction='minimize')
-        study.optimize(lambda trial: DiamondModel.objective(x_train_xbg, y_train_xbg, trial), n_trials=n_trials)
+        study.optimize(lambda trial: DiamondModel.objective(x_train_xgb, y_train_xgb, trial), n_trials=n_trials)
         return study.best_params
 
     @staticmethod
@@ -126,146 +116,116 @@ class DiamondModel:
         raise ValueError(f"Unsupported file extension: {file_path}")
 
     def _load(self, id: str):
-        model_path = f'models/{id}.pkl'
-        if os.path.exists(model_path):
-            self.model = joblib.load(model_path)
+        model_path = f'models/{id}_{self.model_name}.pkl'
+        data_path = f'models/{id}_datas.csv'
+        dummies_path = f'models/{id}_dummies.csv'
+        processed_path = f'models/{id}_processed.csv'
+        
+        if os.path.exists(model_path) and os.path.exists(data_path):
+            self.model_trained = joblib.load(model_path)
+            self.datas = pd.read_csv(data_path)
+            self.datas_dummies = pd.read_csv(dummies_path)
+            self.datas_processed = pd.read_csv(processed_path)
         else:
             raise ValueError("The model does not exist")
 
     def _save(self):
-        joblib.dump(self.model, f'models/{self.id}.pkl')
+        os.makedirs('models', exist_ok=True)
+        joblib.dump(self.model_trained, f'models/{self.id}_{self.model_name}.pkl')
+        self.datas.to_csv(f'models/{self.id}_datas.csv', index=False)
+        self.datas_dummies.to_csv(f'models/{self.id}_dummies.csv', index=False)
+        self.datas_processed.to_csv(f'models/{self.id}_processed.csv', index=False)
 
-    def clean_data(self, columns_to_drop: list[str] = ['depth', 'table', 'y', 'z']):
-        """
-        Clean the data by dropping unnecessary columns and rows with missing values.
-        Params:
-        columns_to_drop: list[str]: The columns to drop from the data.
-        """
+    def clean_data(self, columns_to_drop: list[str] = ['depth', 'table', 'y', 'z'], columns_to_dummies: list[str] = ['cut', 'color', 'clarity']):
         self.datas = self.datas.dropna()
-        self.datas = self.datas[(self.datas.x * self.datas.y * self.datas.z != 0) & (self.datas.price > 0)]
-        self.datas = self.datas.reset_index(drop=True)
+        self.datas = self.datas[(self.datas.x * self.datas.y * self.datas.z != 0) & (self.datas.price > 0)].reset_index(drop=True)
         self.datas_processed = self.datas.drop(columns=columns_to_drop)
-        self.datas_dummies = pd.get_dummies(self.datas_processed)
+        
+        columns = [col for col in columns_to_dummies if col not in columns_to_drop]
+        self.datas_dummies = pd.get_dummies(self.datas_processed, columns=columns, drop_first=True)
 
-    def visualize_scatter_matrix(self, save: bool=False) -> None:
-        """
-        Visualize the correlation between numeric columns in the data.
-        """
-        pd.plotting.scatter_matrix(self.datas.select_dtypes(include=['number']), figsize=(14, 10))
+    def visualize_scatter_matrix(self, save: bool = False):
+        fig = pd.plotting.scatter_matrix(self.datas.select_dtypes(include=['number']), figsize=(14, 10))
         if save:
             plt.savefig(f'visualizations/{self.id}_scatter_matrix.png')
         plt.show()
+        return fig
 
-    def visualize_histogram(self, save: bool=False) -> None:
-        """
-        Visualize the histogram of the data.
-        save : bool: Whether to save the plot or not.
-        """
-        self.datas.hist(bins=100, figsize=(14, 10))
+    def visualize_histogram(self, save: bool = False):
+        fig = self.datas.hist(bins=100, figsize=(14, 10))
         if save:
             plt.savefig(f'visualizations/{self.id}_histogram.png')
         plt.show()
+        return fig
 
-    def visualize_diamond_prices_by(self, cut_column: str,save: bool=False) -> None:
-        """
-        Visualize the prices of diamonds by a specific column.
-        Params:
-        cut_column: str: The column to visualize the prices by.
-        """
+    def visualize_diamond_prices_by(self, cut_column: str, save: bool = False):
         fig = px.violin(self.datas, x=cut_column, y='price', color=cut_column, title=f'Price by {cut_column}')
         fig.show()
         if save:
             fig.write_html(f'visualizations/{self.id}_price_by_{cut_column}.html')
+        return fig
 
     def train_model(self) -> float:
-        """
-        Train the model and return the mean absolute error (MAE).
-        """
         if not hasattr(self, 'datas_dummies'):
             warnings.warn("The data is not yet cleaned, cleaning it now using the default columns to drop")
             self.clean_data()
 
         X = self.datas_dummies.drop(columns=['price'])
         y = self.datas_dummies['price']
-
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         if self.model_name == 'XGBRegressor':
-            best_params = DiamondModel.find_best_hyperparameters(X_train, y_train)
-            self.model = self.model(**best_params, enable_categorical=True, random_state=42)
+            best_params = DiamondModel.find_best_hyperparameters(X_train, y_train, n_trials=100)
+            self.model_trained = self.model(**best_params, enable_categorical=True, random_state=42)
         else:
-            self.model = self.model()
+            self.model_trained = self.model()
 
-        self.model.fit(X_train, y_train)
-        y_pred = self.model.predict(X_test)
+        if self.model_name == "LinearRegression":
+            y_train = np.log(y_train)
+
+        self.model_trained.fit(X_train, y_train)
+
+        if self.model_name == "LinearRegression":
+            y_pred = np.exp(self.model_trained.predict(X_test))
+        else:
+            y_pred = self.model_trained.predict(X_test)
+
         mae = mean_absolute_error(y_test, y_pred)
         self.predictions = y_pred
         self.GT_Y = y_test
         self._save()
         return mae
 
-    def plot_predictions_vs_actual(self, save: bool=False) -> None:
-        """
-        Plot the predictions vs the actual values.
-        save: bool: Whether to save the plot or not.
-        """
+    def plot_predictions_vs_actual(self, save: bool = False):
         DiamondModel.plot_gof(self.GT_Y, self.predictions)
         if save:
             plt.savefig(f'visualizations/{self.id}_predictions_vs_actual.png')
-        
+
     def predict(self, data: pd.DataFrame) -> pd.Series:
-        """
-        Predict the prices of diamonds using the trained model.
-        Params:
-        data: pd.DataFrame: The data to predict the prices of diamonds.
-        """
-        return self.model.predict(data)
-    
+        predictions = self.model_trained.predict(data)
+        if self.model_name == "LinearRegression":
+            predictions = np.exp(predictions)
+        return predictions
+
     def get_similar_samples(self, carat: float, cut: str, color: str, clarity: str, n: int) -> pd.DataFrame:
-        """
-        Get similar samples to a given diamond.
-        Params:
-        carat: float: The carat of the diamond.
-        cut: str: The cut of the diamond.
-        color: str: The color of the diamond.
-        clarity: str: The clarity of the diamond.
-        n: int: The number of similar samples to return.
-        """
         if self.datas is None:
             raise ValueError("Data is not loaded.")
         if carat <= 0 or n <= 0:
             raise ValueError("Carat and n must be greater than 0")
-        if carat>max(self.datas['carat']) or carat<min(self.datas['carat']):
+        if carat > max(self.datas['carat']) or carat < min(self.datas['carat']):
             raise ValueError("Carat must be within the range of the dataset")
-        if n>len(self.datas):
+        if n > len(self.datas):
             return self.datas
-        if clarity not in self.datas['clarity'].unique():
-            raise ValueError("Clarity must be one of the unique values in the dataset")
-        if color not in self.datas['color'].unique():
-            raise ValueError("Color must be one of the unique values in the dataset")
-        if cut not in self.datas['cut'].unique():
-            raise ValueError("Cut must be one of the unique values in the dataset")
-        
-        
-        # Filtra il dataset
-        filtered_data = self.datas[
-            (self.datas['cut'] == cut) &
-            (self.datas['color'] == color) &
-            (self.datas['clarity'] == clarity)
-        ]
-        
-        if filtered_data.empty:
-            return pd.DataFrame()  # Restituisce un DataFrame vuoto se non ci sono campioni simili
+        if clarity not in self.datas['clarity'].unique() or color not in self.datas['color'].unique() or cut not in self.datas['cut'].unique():
+            raise ValueError("Cut, color, and clarity must be one of the unique values in the dataset")
 
-        # Calcola la differenza di peso e ordina per similarità di peso
+        filtered_data = self.datas[(self.datas['cut'] == cut) & (self.datas['color'] == color) & (self.datas['clarity'] == clarity)]
+
+        if filtered_data.empty:
+            return pd.DataFrame()
+
         filtered_data['weight_diff'] = (filtered_data['carat'] - carat).abs()
         similar_samples = filtered_data.sort_values(by='weight_diff').head(n)
-        
-        # Rimuovi la colonna temporanea 'weight_diff'
-        similar_samples = similar_samples.drop(columns=['weight_diff'])
-        
-        return similar_samples
+        return similar_samples.drop(columns=['weight_diff'])
 
-        
-        
-    
+# You can now create an instance of DiamondModel and use it as needed.
---
+++ PipelineInterface.py
@@ -0,0 +1,222 @@
+import streamlit as st
+from DiamondModels import DiamondModel,modelling_algorithms
+import matplotlib.pyplot as plt
+import pandas as pd 
+import os 
+import pandas.api.types as ptypes
+
+existing_model_ids = list(modelling_algorithms.keys())
+
+if "loaded_model" not in st.session_state:
+    st.session_state.loaded_model = False
+if "model_trained" not in st.session_state:
+    st.session_state.model_trained = False
+if "model" not in st.session_state:
+    st.session_state.model = None
+if "columns_to_drop" not in st.session_state:
+    st.session_state.columns_to_drop=[]
+if "prediction" not in st.session_state:
+    st.session_state.prediction = False
+if "success_string" not in st.session_state:
+    st.session_state.success_string = ""
+
+
+st.title("Diamond Price Prediction")
+
+
+models_id={}
+
+def center(col,n):
+    for i in range(n):
+        col.write("     ")
+
+def upload_file(file_upload):
+    file_extension = file_upload.name.split('.')[-1]
+    if file_extension == 'csv':
+        data = pd.read_csv(file_upload)
+    elif file_extension in ['xlsx', 'xls']:
+        data = pd.read_excel(file_upload)
+    elif file_extension == 'json':
+        data = pd.read_json(file_upload)
+    else:
+        st.error("Unsupported file type")
+        st.stop()
+
+    model = DiamondModel(datas=data, model=model_type)
+    st.session_state.model = model
+    st.session_state.model_trained = True
+    
+    st.success(f"Model created successfully with ID: {model.id} and Model: {model_type}")
+    
+    
+def load_model(model_id,model_type):
+    model = DiamondModel(id=model_id, model=model_type)
+    st.session_state.model = model
+    st.session_state.loaded_model = True
+    st.success(f"Model {model_id} loaded successfully")
+    
+if not st.session_state.loaded_model and not st.session_state.model_trained:
+    for i in os.listdir('models'):
+        tmp=i.split('.')[0]
+        tmp_split=tmp.split('_')
+        id=tmp_split[0]
+        type_of_model=tmp_split[1]
+        if i.endswith('.pkl') and f"ID: {id} - Model: {type_of_model}" not in models_id:
+            models_id[f"ID: {id} - Model: {type_of_model}"]=[id,type_of_model]  
+
+
+    file_upload = st.file_uploader("Upload your dataset, remeber that you need the carat, cut, color, clarity, depth, table, price, x, y, z Columns", type=["csv", "xlsx", "json"])
+
+    model_type = st.selectbox("Select the model type", options=["---Select---","LinearRegression", "XGBRegressor"])
+
+    model_id = st.selectbox("Or select an existing model ID", options=["---Select---"] + list(models_id.keys()))
+
+    if st.button("Load or Train Model"):
+        if model_id != "---Select---" and file_upload is not None:
+            st.error("Please upload a file OR select an existing model ID, not both")
+        elif file_upload is not None and model_id=="---Select---":
+            upload_file(file_upload)
+        elif model_id != "---Select---" and file_upload is None:
+            model_type = models_id[model_id][1]
+            model_id = models_id[model_id][0]
+            load_model(model_id,model_type)
+        else:
+            st.error("Please upload a file or select an existing model ID")
+
+if st.session_state.model_trained and not st.session_state.loaded_model and not st.session_state.prediction:
+    col1, col2 = st.columns(2)
+    image_container = st.empty()
+    center(col1,3)
+    col1.write("Want to see the scatter Matrix?")
+    with col2:
+        center(col2,3)  
+        if st.button("Yes",key="scatter"):
+            model:DiamondModel=st.session_state.model
+            fig=model.visualize_scatter_matrix()
+            image_container.pyplot(plt.gcf())
+            
+    center(col1,3)        
+    col1.write("Want to see the histogram matrix?")
+    with col2:
+        center(col2,2)
+        if st.button("Yes",key="histogram"):
+            model:DiamondModel=st.session_state.model
+            fig=model.visualize_histogram()
+            image_container.pyplot(plt.gcf())
+    
+    center(col1,3)
+    col1.write("Want to see the diamond prices by a column?")
+    feature=col1.selectbox("Select the column",options=list(st.session_state.model.datas.columns))
+    with col2:
+        center(col2,2)
+        if st.button("Yes",key="violin"):
+            model:DiamondModel=st.session_state.model
+            fig=model.visualize_diamond_prices_by(feature)
+            image_container.pyplot(plt.gcf(),)
+            
+    center(col1,1)
+    
+    list_of_columns=[col for col in ["Use default setting","Don't want to drop any column"]+list(st.session_state.model.datas.columns) if col not in st.session_state.columns_to_drop]
+    
+    st.markdown("If you want to drop a column, please click the button below")  
+    st.markdown("If you don't want to drop any column, please select 'Don't want to drop any column'")
+    
+    column_to_drop=st.selectbox("Select the column to drop",options=list_of_columns)
+    
+    if st.button("Drop this column",key="train"):
+        if column_to_drop=="Don't want to drop any column":
+            st.session_state.columns_to_drop=[]
+        elif column_to_drop=="Use default setting" or column_to_drop=="Use default setting":
+            st.session_state.columns_to_drop=["Use default setting"]
+        elif column_to_drop!="---Select---":
+            st.session_state.columns_to_drop.append(column_to_drop)
+        
+    if st.session_state.columns_to_drop!=[] and st.session_state.columns_to_drop!=["Default setting"]:
+        st.write(f"Columns to drop: {"   ".join(st.session_state.columns_to_drop)}")
+        
+    if st.button("Train the model",key="train_model"):
+        if column_to_drop=="Use default setting":
+            st.session_state.columns_to_drop=["Use default setting"]
+        if st.session_state.columns_to_drop==[]:
+            st.session_state.model.clean_data(columns_to_drop=[])
+            mae=st.session_state.model.train_model()
+        elif st.session_state.columns_to_drop==["Use default setting"]:
+            st.session_state.model.clean_data()
+            mae=st.session_state.model.train_model()
+        else:
+            st.session_state.model.clean_data(columns_to_drop=st.session_state.columns_to_drop)
+            mae=st.session_state.model.train_model()
+        st.session_state.success_string=f"Model trained successfully with MAE: {mae}"
+        st.session_state.prediction=True
+
+if st.session_state.loaded_model and not st.session_state.model_trained:
+    st.session_state.success_string=f"Model loaded successfully"
+    st.session_state.prediction=True
+    
+
+if st.session_state.prediction and st.session_state.model!=None:
+    st.markdown(f"## {st.session_state.success_string}")
+    st.markdown("### Now is time to make a prediction")
+    model:DiamondModel=st.session_state.model
+    if st.session_state.columns_to_drop==["Use default setting"]:
+        st.session_state.columns_to_drop=['depth', 'table', 'y', 'z']
+        
+    columns_not_dropped=list(set(model.datas_processed.columns)-set(st.session_state.columns_to_drop))
+    columns_not_dropped.remove('price')
+    added={k:list(set(model.datas[k].to_list())) for k in columns_not_dropped if ptypes.is_string_dtype(model.datas[k])}
+    get={
+        
+    }
+    st.write("Please insert the following values")
+    
+    col=st.columns(len(columns_not_dropped))
+    for i in columns_not_dropped:
+        with col[columns_not_dropped.index(i)]:
+            if ptypes.is_string_dtype(model.datas[i]):
+                get[i]=st.selectbox(i.title(),options=added[i])
+            else:
+               get[i]= st.number_input(i.title(),min_value=0.00,step=0.01)
+    if st.button("Predict",key="predict_value"):
+        df=pd.DataFrame([],columns=model.datas_dummies.columns)
+        for i in get:
+            if i in i in ['x','carat']:
+                if get[i]<=0.0:
+                    st.error(f"Please insert a valid value for {i}")
+                    st.stop()
+        df_tmp=pd.DataFrame(get,index=[0])
+        df_tmp=pd.get_dummies(df_tmp,columns=["cut","color","clarity"])
+        for i in df_tmp.columns:
+        
+            if i in df.columns:
+                df[i]=df_tmp[i]
+        df.fillna(False,inplace=True)
+        df.drop(columns=["price"],inplace=True)
+        prediction=model.predict(df)
+        st.markdown(f"""
+
+        ✨ **The probable market value of the diamond is {prediction[0]}** ✨
+
+        💎 This value is calculated based on extensive analysis and our advanced machine learning model. 💎
+
+        _Thank you for using our Diamond Price Prediction service!_
+        """)
+
+          
+        
+                
+    
+    
+    
+if st.button("Go back",key="save"):
+    for i in st.session_state:
+        if i in ["loaded_model","model_trained","model","prediction","success_string"]:
+            st.session_state[i]=None
+        if i=="columns_to_drop":
+            st.session_state.columns_to_drop=[]
+    st.rerun()
+        
+        
+    
+    
+    
+        
